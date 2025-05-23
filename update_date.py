9759aa9551847ca9e7f4c71f0588d736cf033fab
C H A R L E S <charleslei2017@gmail.com>
2025-05-23 15:40:41 +0800

    Update update_date.py

--- update_date.py
+++ update_date.py
@@ -1,7 +1,16 @@
+import requests
 import subprocess
 
-#sudo date MMDDhhmmYYYY
+url         = "https://dev-enmms-api.spphportal.site/api/get-date"
+headers     = {
+                "Authorization": "Bearer 3e13911adffce018d97286bb760dd49146d2a40318542bb1e99219a268f5340a%",
+                "Content-Type": "application/json"
+              }
+response    = request.get(url, headers=headers)
+data        = response.json()
+print(data)
 
+#sudo date MMDDhhmmYYYY
 command = ["sudo","date", "052315262025"]
 
 try:
